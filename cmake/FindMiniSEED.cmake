789116fe921485b88a9f24b930a8d477ca24d154
Ben Baker <bakerb845@gmail.com>
2025-08-26 09:19:54 -0600

    trying to improve finding of seedlink and miniseed

--- cmake/FindMiniSEED.cmake
+++ cmake/FindMiniSEED.cmake
@@ -1,38 +1,53 @@
+#.rst:
+# FindMiniSEED
+# ------------
+# 
+# Finds the MiniSEED library and include.
+#
+# Imported targets
+# ^^^^^^^^^^^^^^^^
+#
+# ``MiniSEED_FOUND``
+#   True indicates MiniSEED was found.
+# ``MiniSEED_VERSION_STRING``
+#   The version found.
+# ``MiniSEED::MiniSEED``
+#   The MiniSEED library, if found.
+
 # Already in cache, be silent
-if (MINISEED_INCLUDE_DIR AND MINISEED_LIBRARY)
-    set(MINISEED_FIND_QUIETLY TRUE)
+if (MiniSEED_INCLUDE_DIR AND MiniSEED_LIBRARY)
+    set(MiniSEED_FIND_QUIETLY TRUE)
 endif()
 
-set(MINISEED mseed)
-
 # Find the include directory
-find_path(MINISEED_INCLUDE_DIR
+find_path(MiniSEED_INCLUDE_DIR
           NAMES libmseed.h
-          HINTS $ENV{MINISEED_ROOT}/include
-                $ENV{MINISEED_ROOT}/
-                /usr/local/include)
+          PATHS /usr/local/include
+                /usr/include
+                "$ENV{MiniSEED_ROOT}/include"
+                "$ENV{MiniSEED_ROOT}/")
 # Find the library components
 if (BUILD_SHARED_LIBS)
    message("Looking for libmseed shared")
-   find_library(MINISEED_LIBRARY
+   find_library(MiniSEED_LIBRARY
                 NAME libmseed.so
-                PATHS $ENV{MINISEED}/lib/
-                      $ENV{MINISEED}/
+                PATHS $ENV{MiniSEED}/lib/
+                      $ENV{MiniSEED}/
                       /usr/local/lib64
                       /usr/local/lib
                )
 else()
    message("Looking for libmseed static")
-   find_library(MINISEED_LIBRARY
+   find_library(MiniSEED_LIBRARY
                 NAME libmseed.a
-                PATHS $ENV{MINISEED}/lib/
-                      $ENV{MINISEED}/
+                PATHS $ENV{MiniSEED}/lib/
+                      $ENV{MiniSEED}/
                       /usr/local/lib64
                       /usr/local/lib
                )
 endif()
-file(STRINGS ${MINISEED_INCLUDE_DIR}/libmseed.h MSEED_HEADER_DATA)
-set(MINISEED_VERSION "")
+file(STRINGS ${MiniSEED_INCLUDE_DIR}/libmseed.h MSEED_HEADER_DATA)
+set(MiniSEED_VERSION "")
 while (MSEED_HEADER_DATA)
   list(POP_FRONT MSEED_HEADER_DATA LINE)
   if (LINE MATCHES "#define LIBMSEED_VERSION")
@@ -40,14 +55,14 @@ while (MSEED_HEADER_DATA)
      string(REPLACE "#define LIBMSEED_VERSION " "" LINE ${LINE})
      string(REPLACE "\"" "" LINE ${LINE})
      string(REPLACE "//!< Library version" "" LINE ${LINE})
-     string(STRIP LINE ${LINE})
+     string(STRIP ${LINE} LINE)
      set(LINE_LIST ${LINE})
-     list(GET LINE_LIST 0 MINISEED_VERSION)
+     list(GET LINE_LIST 0 MiniSEED_VERSION)
      #string(COMPARE GREATER ${VERSION} "3.1.5" RESULT) 
      #if (RESULT)
-     #   set(MINISEED_VERSION ${VERSION})
+     #   set(MiniSEED_VERSION ${VERSION})
      #endif()
-     message("Extracted mseed version ${MINISEED_VERSION}")
+     message("Extracted mseed version ${MiniSEED_VERSION}")
      break()
   endif()
 endwhile()
@@ -55,5 +70,18 @@ endwhile()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MiniSEED
-                                  DEFAULT_MSG MINISEED_INCLUDE_DIR MINISEED_LIBRARY MINISEED_VERSION)
-mark_as_advanced(MINISEED_INCLUDE_DIR MINISEED_LIBRARY MINISEED_VERSION)
+                                  FOUND_VAR MiniSEED_FOUND
+                                  REQUIRED_VARS MiniSEED_LIBRARY MiniSEED_INCLUDE_DIR
+                                  VERSION_VAR ${MiniSEED_VERSION})
+if (MiniSEED_FOUND AND NOT TARGET MiniSEED::MiniSEED)
+   add_library(MiniSEED::MiniSEED UNKNOWN IMPORTED)
+   set_target_properties(MiniSEED::MiniSEED PROPERTIES
+                         IMPORTED_LOCATION "${MiniSEED_LIBRARY}"
+                         INTERFACE_INCLUDE_DIRECTORIES "${MiniSEED_INCLUDE_DIR}")
+endif()
+mark_as_advanced(MiniSEED_INCLUDE_DIR MiniSEED_LIBRARY)
+
+#include(FindPackageHandleStandardArgs)
+#find_package_handle_standard_args(MiniSEED
+#                                  DEFAULT_MSG MINISEED_INCLUDE_DIR MINISEED_LIBRARY MINISEED_VERSION)
+#mark_as_advanced(MINISEED_INCLUDE_DIR MINISEED_LIBRARY MINISEED_VERSION)
--- cmake/FindSEEDLink.cmake
+++ cmake/FindSEEDLink.cmake
@@ -1,38 +1,105 @@
+#.rst:
+# FindSEEDLink
+# ------------
+# 
+# Finds the SEEDLink library and include.
+#
+# Imported targets
+# ^^^^^^^^^^^^^^^^
+#
+# ``SEEDLink_FOUND``
+#   True indicates SEEDLink was found.
+# ``SEEDLink_VERSION_STRING``
+#   The version found.
+# ``SEEDLink::SEEDLink``
+#   The SEEDLink library, if found. 
+
 # Already in cache, be silent
-if (SEEDLINK_INCLUDE_DIR AND SEEDLINK_LIBRARY)
-    set(SEEDLINK_FIND_QUIETLY TRUE)
+if (SEEDLink_INCLUDE_DIR AND SEEDLink_LIBRARY)
+    set(SEEDLink_FIND_QUIETLY TRUE)
 endif()
 
 # Find the include directory
-find_path(SEEDLINK_INCLUDE_DIR
+find_path(SEEDLink_INCLUDE_DIR
           NAMES libslink.h
-          HINTS $ENV{SEEDLINK_ROOT}/include
-                $ENV{SEEDLINK_ROOT}/
-                /usr/local/include
-          )
+          PATHS /usr/local/include
+                /usr/include
+                "$ENV{SEEDLink_ROOT}/include"
+                "$ENV{SEEDLink_ROOT}")
 # Find the library components
 if (BUILD_SHARED_LIBS)
-   message("Looking for libslink shared")
-   find_library(SEEDLINK_LIBRARY
+   message("Looking for libslink shared library")
+   find_library(SEEDLink_LIBRARY
                 NAMES libslink.so
-                PATHS $ENV{SEEDLINK_ROOT}/lib/
-                      $ENV{SEEDLINK_ROOT}/
+                PATHS /usr/local/lib
                       /usr/local/lib64
-                      /usr/local/lib
+                      "$ENV{SEEDLink_ROOT}/lib/"
+                      "$ENV{SEEDLink_ROOT}/"
                 )
 else()
-   message("Looking for libslink static")
-   find_library(SEEDLINK_LIBRARY
+   message("Looking for libslink static library")
+   find_library(SEEDLink_LIBRARY
                 NAME libslink.a
-                PATHS $ENV{SEEDLINK_ROOT}/lib/
-                      $ENV{SEEDLINK_ROOT}/
+                PATHS /usr/local/lib
                       /usr/local/lib64
-                      /usr/local/lib
-                ) 
+                      "$ENV{SEEDLink_ROOT}/lib/"
+                      "$ENV{SEEDLink_ROOT}/"
+               )
 endif()
-# Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if
-# all listed variables are TRUE.
+
+file(STRINGS ${SEEDLink_INCLUDE_DIR}/libslink.h LIBSLINK_HEADER_DATA)
+set(SEEDLink_VERSION "")
+set(SEEDLink_VERSION_MAJOR "")
+set(SEEDLink_VERSION_MINOR "")
+set(SEEDLink_VERSION_PATCH "")
+while (LIBSLINK_HEADER_DATA)
+  list(POP_FRONT LIBSLINK_HEADER_DATA LINE)
+  if (LINE MATCHES "#define LIBSLINK_VERSION_MAJOR")
+     #message("Found libslink major version line: " ${LINE})
+     string(REPLACE "#define LIBSLINK_VERSION_MAJOR " "" LINE ${LINE})
+     string(REPLACE "\"" "" LINE ${LINE})
+     string(REPLACE "/**< libslink major version */" "" LINE ${LINE})
+     string(STRIP ${LINE} LINE)
+     set(LINE_LIST ${LINE})
+     list(GET LINE_LIST 0 SEEDLink_VERSION_MAJOR)
+     #message("libslink patch version ${SEEDLink_VERSION_MAJOR}")
+  endif()
+  if (LINE MATCHES "#define LIBSLINK_VERSION_MINOR")
+     #message("Found libslink minor version line: " ${LINE})
+     string(REPLACE "#define LIBSLINK_VERSION_MINOR " "" LINE ${LINE})
+     string(REPLACE "\"" "" LINE ${LINE})
+     string(REPLACE "/**< libslink minor version */" "" LINE ${LINE})
+     string(STRIP ${LINE} LINE)
+     set(LINE_LIST ${LINE})
+     list(GET LINE_LIST 0 SEEDLink_VERSION_MINOR)
+     #message("libslink minor version ${SEEDLink_VERSION_MINOR}")
+  endif()
+  if (LINE MATCHES "#define LIBSLINK_VERSION_PATCH")
+     #message("Found libslink patch version line: " ${LINE})
+     string(REPLACE "#define LIBSLINK_VERSION_PATCH " "" LINE ${LINE})
+     string(REPLACE "/**< libslink patch version */" "" LINE ${LINE})
+     string(STRIP ${LINE} LINE)
+     set(LINE_LIST ${LINE})
+     list(GET LINE_LIST 0 SEEDLink_VERSION_PATCH)
+  endif()
+  if (NOT ${SEEDLink_VERSION_MAJOR} STREQUAL "" AND
+      NOT ${SEEDLink_VERSION_MINOR} STREQUAL "" AND
+      NOT ${SEEDLink_VERSION_PATCH} STREQUAL "")
+     set(SEEDLink_VERSION "${SEEDLink_VERSION_MAJOR}.${SEEDLink_VERSION_MINOR}.${SEEDLink_VERSION_PATCH}")
+     message("Extracted SEEDLink version: ${SEEDLink_VERSION}")
+     break()
+  endif()
+endwhile()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SEEDLink
-                                  DEFAULT_MSG SEEDLINK_INCLUDE_DIR SEEDLINK_LIBRARY)
-mark_as_advanced(SEEDLINK_INCLUDE_DIR SEEDLINK_LIBRARY)
+                                  FOUND_VAR SEEDLink_FOUND
+                                  REQUIRED_VARS SEEDLink_LIBRARY SEEDLink_INCLUDE_DIR
+                                  VERSION_VAR ${SEEDLink_VERSION})
+if (SEEDLink_FOUND AND NOT TARGET SEEDLink::SEEDLink)
+   add_library(SEEDLink::SEEDLink UNKNOWN IMPORTED)
+   set_target_properties(SEEDLink::SEEDLink PROPERTIES
+                         IMPORTED_LOCATION "${SEEDLink_LIBRARY}"
+                         INTERFACE_INCLUDE_DIRECTORIES "${SEEDLink_INCLUDE_DIR}")
+endif()
+mark_as_advanced(SEEDLink_INCLUDE_DIR SEEDLink_LIBRARY)
